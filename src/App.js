88a261fc0ab96476a3c00da355495a1c2fd159aa
shivanshu32 <shivanshu32@gmail.com>
2022-04-03 15:50:18 +0530

    first upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './pages/dashboard'
+
+
+function App() {
+  return (
+    <div>
+    <Dashboard />
+    </div>
+  );
+}
+
+export default App;
